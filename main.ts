c452168dced9db581d8cf276c4ec1a2f92e92501
Abdallah <153225437+riditIcu@users.noreply.github.com>
2025-05-14 16:34:46 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Number1 = 0
+let Number2 = 0
+input.onButtonPressed(Button.A, function () {
+    Number1 += 1
+    basic.showNumber(Number1)
+})
+input.onButtonPressed(Button.B, function () {
+    Number2 += 1
+    basic.showNumber(Number2)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(Number1 + Number2)
+})
